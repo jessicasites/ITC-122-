779bca2774a7c7d3733da3a27ea081cf446cd1b1
jessicasites <56246446+jessicasites@users.noreply.github.com>
2020-07-14 01:49:30 -0700

    update index file

--- index.js
+++ index.js
@@ -1,5 +1,70 @@
-/*index.js - with code to launch a node.js web server on port 3000 and respond to requests.
-Should import your data.js module */
+/*  Second assignment:
+    Convert your index.js code to use Express syntax,
+Update your default route to show formatted information for each item in your data array:
+create home.handlebars template
+render this template for requests to your default route
+pass your data array to the handlebars template
+display the list of items in your data array 
+each item is linked to your detail page with a query parameter identifying the item
+*/
+// Create variable for http and data
+// Define express 
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const employees = require("./data");
+
+const app = express();
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({
+    defaultLayout: false
+}));
+
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+// Set location 
+app.use(express.static(__dirname + '/public')); 
+
+// Parse form submissions
+app.use(bodyParser.urlencoded({extended: true})); 
+
+// Create variable to get info from data.js
+let ListEmployees = employees.getAll();
+
+//Route to home 
+app.get('/', (req, res) => {
+  res.render('home', {employees: ListEmployees});
+});
+
+//Route to the detail
+app.get('/detail', (req, res) => {
+  const firstName = req.query.firstName
+  res.render('detail', {firstName:firstName, stats: employees.getDetail(firstName)});
+});
+
+// Text response
+app.get('/about', (req, res) => {
+  res.type('text/plain');
+  res.send('About page \n I am a student at SCCC');
+});
+    
+// 404 message
+app.use( (req, res) => {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Express started');
+});
+
+/*First assignment:  
+index.js - with code to launch a node.js web server on port 3000 and respond to requests.
+Should import your data.js module 
 
 // Create variable for http and data
 const http = require("http"); 
@@ -31,4 +96,4 @@ http.createServer(function(req,res) {
             break;
           }
     }
- ).listen(process.env.PORT || 3000);
+ ).listen(process.env.PORT || 3000); */
