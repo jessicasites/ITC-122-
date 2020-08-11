e8cb03c0555a74d2457948a98b9a3447ee7ef64f
jessicasites <56246446+jessicasites@users.noreply.github.com>
2020-08-10 18:21:16 -0700

    assignment 4

---
+++ models/employee.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const credentials = require("../lib/credentials");
+
+// local db connection settings 
+// const ip = process.env.ip || '127.0.0.1';
+// const connectionString = 'mongodb://' +ip+ '/<DB_NAME>';
+
+mongoose.connect(credentials.connectionString, { dbName: 'sccprojects', useNewUrlParser: true }); 
+
+mongoose.connection.on('open', () => {
+  console.log('Mongoose connected.');
+});
+
+// define employee model in JSON key/value pairs
+// values indicate the data type of each key
+const mySchema = mongoose.Schema({
+ title: { type: String, required: true },
+ firstName: String,
+ lastName: String,
+ startDate: Date,
+ status: String
+}); 
+
+module.exports = mongoose.model('Employee', mySchema);
